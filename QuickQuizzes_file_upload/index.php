0781298c6023976527b88e1148c4cd87dafd4d93
NIKHIL CHAURASIYA <62669831+nik9327@users.noreply.github.com>
2020-12-24 23:15:22 +0530

    Add files via upload

---
+++ QuickQuizzes_file_upload/index.php
@@ -0,0 +1,30 @@
+<?php 
+include('header2.php');
+include_once("db_connect.php");
+?>
+<title> Multiple Images </title>
+<script type="text/javascript" src="scripts/jquery.form.js"></script>
+<script type="text/javascript" src="scripts/upload.js"></script>
+<link type="text/css" rel="stylesheet" href="style.css" />
+<link type="text/css" rel="stylesheet" href="style.css" />
+<div class="container">
+		
+<!--
+	<form method="post" name="upload_form" id="upload_form" enctype="multipart/form-data" action="upload.php">   
+    <label>Choose Multiple Images to Upload</label>
+	<br>
+	<br>
+    <input type="file" name="upload_images[]" id="image_file" multiple >
+    <div class="file_uploading hidden">
+        <label>&nbsp;</label>
+        <img src="uploading.gif" alt="Uploading......"/>
+    </div>
+	</form>
+	<div id="uploaded_images_preview"></div>
+
+	<br>	
+	<div style="margin:50px 0px 0px 0px;">
+		<a class="btn btn-default read-more" style="background:#3399ff;color:white">Upload all files</a>		
+	</div>
+-->
+</div>
