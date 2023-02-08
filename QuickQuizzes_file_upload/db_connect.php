78f4e0e324b9a4bcb10e28805d7014725a79b5d2
kaysmart001 <luwoyekayode99@gmail.com>
2023-02-08 11:07:35 +0100

    Update db_connect.php

--- QuickQuizzes_file_upload/db_connect.php
+++ QuickQuizzes_file_upload/db_connect.php
@@ -1,9 +1,9 @@
 <?php
 /* Database connection start */
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "upload";
+$servername = "sql105.epizy.com";
+$username = "epiz_33070717";
+$password = "WBtcNlYDj3Y5qkD";
+$dbname = "epiz_33070717_cbt_db";
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
